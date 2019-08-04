63307ec3b72908f33cf6e4411d754a838c7bf2b7
Sam Der <samder2468@gmail.com>
2019-08-04 08:14:25 -0700

    Updates

---
+++ precache-manifest.581e748de542dd604d01affef0fb480f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1e11892dd72eb561b86b4fe9217970",
+    "url": "/EHSMUN/index.html"
+  },
+  {
+    "revision": "1f748862e3579b317670",
+    "url": "/EHSMUN/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1692a3517ef4143ccac3",
+    "url": "/EHSMUN/static/css/main.9f9ed2ed.chunk.css"
+  },
+  {
+    "revision": "1f748862e3579b317670",
+    "url": "/EHSMUN/static/js/2.488ee1e0.chunk.js"
+  },
+  {
+    "revision": "1692a3517ef4143ccac3",
+    "url": "/EHSMUN/static/js/main.ae480180.chunk.js"
+  },
+  {
+    "revision": "a79844ddc35e5c61b4a6",
+    "url": "/EHSMUN/static/js/runtime~main.e2d68985.js"
+  },
+  {
+    "revision": "d6ef657e4f0dafa29200d4ab378b5f38",
+    "url": "/EHSMUN/static/media/ucberkeley.d6ef657e.jpg"
+  }
+]);
