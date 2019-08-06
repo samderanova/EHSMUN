62ca00d78e8d72d62d6d3d36feca04c5be9a414a
Sam Der <samder2468@gmail.com>
2019-08-06 12:47:16 -0700

    fixed more footer problems, added a lot more pictures, implemeneted lazy loading of pictures

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,51 +1,54 @@
 import React from 'react';
 import './Gallery.css';
 import 'bootstrap/dist/css/bootstrap.css';  
+import lozad from 'lozad';
 
 function Gallery () {
+    function lazyLoad() {
+        const observer = lozad();
+        observer.observe();
+    }
     return (
-        <div className="App slide-in">
+        <div className="App slide-in" onLoad={lazyLoad}>
             <div className="body">
                 <div className="row">
+
                     <div className="column">
                         <img src={'nhsmun5.jpeg'} alt='group pic'/>
-                        <div className="column">    
-                            <img src={'nhsmun7.jpg'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='large conference room'/>
-                            <img src={'nhsmun2.jpeg'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='funny pic' />
+                        <div className="column">
+                            <img src={'nhsmun7.jpg'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='un'/>
+                            <img src={'nhsmun2.jpeg'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='discussion' />
                         </div>
-                        <img src={'nhsmun1.jpeg'} alt='group pic'/>
-                        <img src={'IMG_6759.JPG'} alt='alameda point'/>
+                        <img className="lozad" data-src={'IMG_6839.jpg'} alt='funny pic'/>
+                        <div className="column">
+                            <img className="lozad" data-src={'IMG_6806.JPG'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='view of berkeley'/>
+                            <img className="lozad" data-src={'IMG_6759.JPG'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='alameda point'/>
+                        </div>
+                        <img className="lozad" data-src={'IMG_6771.JPG'} alt="oxford" />
                     </div>
+                    
                     <div className="column">
-                        <img src={'IMG_6839.jpg'} alt='discussion'/>
-                        <img src={'IMG_6837.jpg'} alt='un'/>
-                        <img src={'IMG_6827.JPG'} alt='group waiting'/>
+                        <img src={'IMG_8237.JPG'} alt='flight' />
+                        <img src={'nhsmun1.jpeg'} alt='large conference room'/>
+                        <img className="lozad" data-src={'nhsmun3.jpeg'} alt='hallway'/>
+                        <img className="lozad" data-src={'IMG_6837.jpg'} alt='group pic'/>
                     </div>
+
                     <div className="column">
                         <img src={'nhsmun4.jpeg'} alt='walking around'/>
                         <div className="column">
-                            <img src={'IMG_6806.JPG'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='view of berkeley'/>
-                            <img src={'IMG_6767.JPG'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='group waiting' />
+                            <img src={'nhsmun6.jpeg'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='new york' />
+                            <img src={'nhsmun8.jpg'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='new york night' />
                         </div>
-                        <img src={'IMG_6760.JPG'} alt='bart station'/>
+                        <img className="lozad" data-src={'IMG_6767.JPG'} alt='group waiting' />
+                        <div className="column">
+                            <img className="lozad" data-src={'IMG_6760.JPG'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='bart station'/>
+                            <img className="lozad" data-src={'IMG_6827.JPG'} style={{maxWidth: "50%", paddingRight: "4px", verticalAlign: "top"}} alt='group waiting'/>
+                        </div>
+                        <img className="lozad" data-src={'IMG_6831.jpg'} alt='oakland' />
                     </div>
+
                 </div>
-                {/*<div className="nhsmun2019">
-                    <h5>National High School Model United Nations (NHSMUN) 2019</h5>
-                    <img src={'nhsmun1.jpeg'} alt='large conference room'/>
-                    <img src={'nhsmun2.jpeg'} alt='discussion'/>
-                    <img src={'nhsmun4.jpeg'} alt='walking around'/>
-                    <img src={'nhsmun5.jpeg'} alt='group pic'/>
-                    <img src={'nhsmun7.jpg'} alt='un'/>
-                </div>
-                <div className="bmun2019">
-                    <h5>Berkeley Model United Nations (BMUN) 2019</h5>
-                    <img src={'IMG_6837.jpg'} alt='group pic'/>
-                    <img src={'IMG_6827.JPG'} alt='group waiting'/>
-                    <img src={'IMG_6806.JPG'} alt='view of berkeley'/>
-                    <img src={'IMG_6760.JPG'} alt='bart station'/>
-                    <img src={'IMG_6759.JPG'} alt='alameda point'/>
-                </div>*/}
             </div>
         </div>
     );
