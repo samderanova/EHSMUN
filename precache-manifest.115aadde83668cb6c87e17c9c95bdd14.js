3273196b5597b19684455387f1bb1c9c91018827
Sam Der <samder2468@gmail.com>
2019-10-19 15:57:54 -0700

    Updates

--- precache-manifest.115aadde83668cb6c87e17c9c95bdd14.js
+++ precache-manifest.115aadde83668cb6c87e17c9c95bdd14.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "73314a7c59a6506cdb8858dd6be3b99b",
+    "revision": "00c4912e4a3ffa3c043fd3e2f2ed1dca",
     "url": "/EHSMUN/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/EHSMUN/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "7a767a7e9795f69e083e",
-    "url": "/EHSMUN/static/css/main.513b07f2.chunk.css"
+    "revision": "37ef6509ab8b8bb90bea",
+    "url": "/EHSMUN/static/css/main.88477d5b.chunk.css"
   },
   {
     "revision": "5a2530f090e2219105c2",
     "url": "/EHSMUN/static/js/2.f1e8fec5.chunk.js"
   },
   {
-    "revision": "7a767a7e9795f69e083e",
-    "url": "/EHSMUN/static/js/main.f97e92e1.chunk.js"
+    "revision": "37ef6509ab8b8bb90bea",
+    "url": "/EHSMUN/static/js/main.f76a0cba.chunk.js"
   },
   {
     "revision": "a79844ddc35e5c61b4a6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EHSMUN/precache-manifest.c1b639aa9262e1884d69ad8052a0d672.js"
+  "/EHSMUN/precache-manifest.115aadde83668cb6c87e17c9c95bdd14.js"
 );
 
 self.addEventListener('message', (event) => {
