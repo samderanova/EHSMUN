dad280e034fa0935ffbfb62197852d2a1dc8ec54
Sam Der <samder2468@gmail.com>
2019-10-19 17:07:04 -0700

    commitMessage

--- gitPusher.bat
+++ gitPusher.bat
@@ -4,6 +4,6 @@ call npm run deploy
 cd ..
 call git init
 call git add .
-set /p commitMessage = Enter the commit message in quotation marks
+set /p commitMessage = Enter the commit message in quotation marks:
 call git commit -m "%commitMessage"
 call git push -u origin master
